91155a69467e01b88f8b2c046e3c1010bc65fba5
alexandru.patroi <alexandru.patroi@opswat.com>
2023-05-25 12:44:17 +0300

    fix

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,7 +5,7 @@ variable "subnet_cidr_block" {
     default = "10.0.0.0/24"
 }
 variable "avail_zone" {
-    default = "eu-central-1"
+    default = "eu-central-1a"
 }
 variable "env_prefix" {
     default = "dev"
